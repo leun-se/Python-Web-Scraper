774e84b58593e3f6c4371b10620fe5a555f7bdcb
Brian Lee <leun.se781@gmail.com>
2025-09-04 19:33:05 -0400

    redid setup to be in virtual env

--- playwright_scraper/playwright_scraper/settings.py
+++ playwright_scraper/playwright_scraper/settings.py
@@ -85,14 +85,11 @@ DOWNLOAD_DELAY = 1
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
-
 DOWNLOAD_HANDLERS = {
-        "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-        "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    }
-
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-
 PLAYWRIGHT_LAUNCH_OPTIONS = {
     "headless": False,
 } 
